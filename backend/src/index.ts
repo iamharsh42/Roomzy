2283c0bf2920a9364c7049f53210021fe22b2165
Harsh <tiwariharsh421@gmail.com>
2024-06-07 13:00:50 +0530

    layouts added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose'; // lets us connect to database and interact with it
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express() // creates an express app
 app.use(express.json()) // helps convert the body of api request to convert to json format automatically
