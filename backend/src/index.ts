c88b2e019d58ae845c7a1c387f05f7f126a54945
Harsh <tiwariharsh421@gmail.com>
2024-06-12 12:29:03 +0530

    login backend added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose'; // lets us connect to database and interact with it
 import userRoutes from './routes/users';
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -11,6 +12,7 @@ app.use(express.json()) // helps convert the body of api request to convert to j
 app.use(express.urlencoded({extended: true})) // helps parse end url
 app.use(cors()) 
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(5000, ()=>{
