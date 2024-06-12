bdb459d7ec2b8e762ede969073606dfc967ced56
Harsh <tiwariharsh421@gmail.com>
2024-06-12 13:42:04 +0530

    register frontend added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layout/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/search" element={<Layout> 
         <p>Search Page</p>
         </Layout>} />
+        <Route path="/register" element={<Layout><Register/></Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
