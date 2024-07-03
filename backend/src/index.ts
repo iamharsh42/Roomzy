c878e07f498b50e5a30b357306eb0ac88cc913f2
Harsh <tiwariharsh421@gmail.com>
2024-07-03 13:52:25 +0530

    cloudinary added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,13 @@ import authRoutes from "./routes/auth";
 import cookieParser from "cookie-parser";
 import { cookie } from 'express-validator';
 import path from "path";
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
