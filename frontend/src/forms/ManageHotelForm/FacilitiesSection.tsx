15e30eaca4c35d493189f8e6a1bdbe204f891d5a
Harsh <tiwariharsh421@gmail.com>
2024-07-09 13:09:44 +0530

    add hotel section updated

--- frontend/src/forms/ManageHotelForm/FacilitiesSection.tsx
+++ frontend/src/forms/ManageHotelForm/FacilitiesSection.tsx
@@ -11,11 +11,22 @@ const FacilitiesSection = () => {
             <div className="grid grid-cols-5 gap-3">
                 {hotelFacilities.map((facility) => (
                     <label className="text-sm flex gap-1 text-gray-700">
-                        <input type="checkbox" value={facility} />
+                        <input type="checkbox" value={facility} {...register("facilities", {
+                            validate: (facilities) => {
+                                if (facilities && facilities.length > 0) {
+                                    return true;
+                                } else {
+                                    return "At least one facility is required";
+                                }
+                            },
+                        })} />
                         {facility}
                     </label>
                 ))}
             </div>
+            {errors.facilities && (
+                <span className="text-red-500 text-sm font-bold">{errors.facilities.message}</span>
+            )}
         </div>
     )
 };
--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -2,6 +2,8 @@ import { useForm, FormProvider } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
 import TypeSection from "./TypeSection";
 import FacilitiesSection from "./FacilitiesSection";
+import GuestsSection from "./GuestsSection";
+import ImagesSection from "./ImagesSection";
 
 export type HotelFormData = {
     name: string;
@@ -19,12 +21,39 @@ export type HotelFormData = {
 
 const ManageHotelForm = () => {
     const fromMethods = useForm<HotelFormData>();
+    const { handleSubmit } = fromMethods;
+
+    const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+        const formData = new FormData();
+        formData.append("name", formDataJson.name);
+        formData.append("city", formDataJson.city);
+        formData.append("country", formDataJson.country);
+        formData.append("description", formDataJson.description);
+        formData.append("text", formDataJson.type);
+        formData.append("pricePerNight", formDataJson.pricePerNight.toString());
+        formData.append("starRating", formDataJson.starRating.toString());
+        formData.append("adultCount", formDataJson.adultCount.toString());
+        formData.append("childCount", formDataJson.childCount.toString());
+
+        formDataJson.facilities.forEach((facility, index) => {
+            formData.append(`facilities[${index}]`, facility)
+        })
+
+        Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+            formData.append(`imageFiles`, imageFile);
+        })
+    });
     return (
         <FormProvider {...fromMethods}>
-            <form className="flex flex-col gap-10">
+            <form className="flex flex-col gap-10" onSubmit={onSubmit}>
                 <DetailsSection />
                 <TypeSection />
                 <FacilitiesSection />
+                <GuestsSection />
+                <ImagesSection />
+                <span className="flex justify-end">
+                    <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">Save</button>
+                </span>
             </form>
         </FormProvider>
     )
