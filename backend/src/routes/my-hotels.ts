8d8b12b8259c9cc23c8be0e1f652f0c9a13a2e8e
Harsh <tiwariharsh421@gmail.com>
2024-07-12 23:35:23 +0530

    new tests added

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import multer from 'multer';
 import cloudinary from "cloudinary";
-import Hotel, { HotelType } from '../models/hotel';
+import Hotel from '../models/hotel';
+import { HotelType } from '../shared/types';
 import verifyToken from '../middleware/auth';
 import { body } from "express-validator";
 
@@ -62,6 +63,20 @@ router.get("/", verifyToken, async (req: Request, res: Response) => {
     try {
         const hotels = await Hotel.find({ userId: req.userId });
         res.json(hotels);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching hotels" });
+    }
+});
+
+router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+    const id = req.params.id.toString();
+    try {
+        const hotel = await Hotel.find({
+            _id: id,
+            userId: req.userId,
+
+        });
+        res.json(hotel);
     } catch (error) {
         res.status(500).json({ message: "Error fetching hotels" });
     }
