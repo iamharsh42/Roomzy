2ebfe7b1c1590877fea12e1ebcd71b2bf365eb44
Harsh <tiwariharsh421@gmail.com>
2024-07-16 23:24:26 +0530

    search bar added

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 type SearchContext = {
     destination: string;
@@ -22,20 +22,37 @@ export const SearchContextProvider = ({ children }: SearchContextProviderProps) 
     const [checkOut, setCheckOut] = useState<Date>(new Date());
     const [adultCount, setAdultCount] = useState<number>(1);
     const [childCount, setChildCount] = useState<number>(0);
+    const [hotelId, setHotelId] = useState<string>("");
 
     const saveSearchValues = (
-        destination: string, checkIn: Date, checkOut: Date, adultCount: number, childCount: number
+        destination: string, checkIn: Date, checkOut: Date, adultCount: number, childCount: number, hotelId?: string
     ) => {
         setDestination(destination);
         setCheckIn(checkIn);
         setCheckOut(checkOut);
         setAdultCount(adultCount);
         setChildCount(childCount);
-    }
+        if (hotelId) {
+            setHotelId(hotelId);
+        }
+    };
 
     return (
-        <SearchContext.Provider value={{}}>
+        <SearchContext.Provider value={{
+            destination,
+            checkIn,
+            checkOut,
+            adultCount,
+            childCount,
+            hotelId,
+            saveSearchValues,
+        }}>
             {children}
         </SearchContext.Provider>
-    )
-}
+    );
+};
+
+export const useSearchContext = () => {
+    const context = useContext(SearchContext);
+    return context as SearchContext;
+};
