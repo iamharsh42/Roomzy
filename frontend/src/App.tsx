7d6b1ea37c627d22830690d6fe33c8726a45c9d2
Harsh <tiwariharsh421@gmail.com>
2024-07-17 14:19:37 +0530

    search bar completed(frontend)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -21,7 +22,7 @@ const App = () => {
           <p>Home Page</p>
         </Layout>} />
         <Route path="/search" element={<Layout>
-          <p>Search Page</p>
+          <Search />
         </Layout>} />
         <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="/sign-in" element={<Layout><SignIn /></Layout>} />
--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header"
 import Hero from "../components/Hero";
+import SearchBar from "../components/SearchBar";
 
-interface Props{
+interface Props {
     children: React.ReactNode;
 }
 
-const Layout =({children}: Props)=>{
-    return(
+const Layout = ({ children }: Props) => {
+    return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <Hero />
+            <div className="container mx-auto">
+                <SearchBar />
+            </div>
             <div className="container mx-auto py-10 flex-1">
                 {children}
             </div>
