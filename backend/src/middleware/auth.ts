29c347159462c1e08f335fb957138b11de941411
Harsh <tiwariharsh421@gmail.com>
2024-07-20 21:26:33 +0530

    filters updated

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -11,16 +11,16 @@ declare global {
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies["auth_token"];
-    if(!token) {
-        return res.status(401).json({message: "unauthorized"});
+    if (!token) {
+        return res.status(401).json({ message: "unauthorized" });
     } // if token is not present then send unauthorized message
 
-    try{
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
         req.userId = (decoded as JwtPayload).userId;
         next();
-    }catch(error){
-        return res.status(401).json({message: "unauthorized"});
+    } catch (error) {
+        return res.status(401).json({ message: "unauthorized" });
     }
 };
 
