13c01a84fa9f77f34473faac427a6d12c478fc6a
Harsh <tiwariharsh421@gmail.com>
2024-07-27 21:30:05 +0530

    minor update

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import Toast from "../components/Toast";
 import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+const STRIPE_PUB_KEY = import.meta.env.VITE_PUB_KEY || "";
 
 type ToastMessage = {
     message: string;
@@ -12,9 +15,12 @@ type ToastMessage = {
 type AppContext = {
     showToast: (toastMessage: ToastMessage) => void;
     isLoggedIn: boolean;
+    stripePromise: Promise<Stripe | null>;
 }
 
 const AppContext = React.createContext<AppContext | undefined>(undefined); // created a context with default value "undefined"
+
+const stripePromise = loadStripe(STRIPE_PUB_KEY);
 
 export const AppContextProvider = ({
     children,
@@ -31,7 +37,8 @@ export const AppContextProvider = ({
             showToast: (toastMessage) => {
                 setToast(toastMessage);
             },
-            isLoggedIn: !isError
+            isLoggedIn: !isError,
+            stripePromise
         }}>
             {toast && (
                 <Toast
