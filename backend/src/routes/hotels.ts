52235d1188a18636a38d0cbec43e772bee0fc2ec
Harsh <tiwariharsh421@gmail.com>
2024-07-29 19:26:36 +0530

    new tests for stripe added

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -90,7 +90,7 @@ router.post("/:hotelId/bookings/payment-intent", verifyToken, async (req: Reques
         const totalCost = hotel.pricePerNight * numberOfNights;
 
         const paymentIntent = await stripe.paymentIntents.create({
-            amount: totalCost,
+            amount: totalCost * 100,
             currency: "gbp",
             metadata: {
                 hotelId,
--- e2e-tests/tests/search-hotel.spec.ts
+++ e2e-tests/tests/search-hotel.spec.ts
@@ -42,3 +42,30 @@ test("should show hotel detail", async ({ page }) => {
     await expect(page).toHaveURL("/detail/");
     await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
 });
+
+test("should book hotel", async ({ page }) => {
+    page.goto(UI_URL);
+
+    await page.getByPlaceholder("Where are you going?").fill("Dublin");
+    const date = new Date();
+    date.setDate(date.getDate() + 3);
+    const formattedDate = date.toISOString().split("T")[0]; // this converts the date to a human readable format.
+    await page.getByPlaceholder("Check-out Date").fill(formattedDate);
+
+
+    await page.getByRole("button", { name: "Search" }).click();
+
+    await page.getByText("Dublin Getaways").click();
+    await page.getByRole("button", { name: "Book now" }).click();
+
+    await expect(page.getByText("Total Cost: £357.00")).toBeVisible();
+
+    const stripeFrame = page.frameLocator("iframe").first();
+    await stripeFrame.locator('[placeholder="Card number"]').fill("4242424242424242");
+    await stripeFrame.locator('[placehlder="MM / YY"').fill("04/30");
+    await stripeFrame.locator('[placehlder="CVC"').fill("242");
+    await stripeFrame.locator('[placehlder="ZIP"').fill("24225");
+
+    await page.getByRole("button", { name: "Confirm Booking" }).click();
+    await expect(page.getByText("Booking Saved!")).toBeVisible();
+});
