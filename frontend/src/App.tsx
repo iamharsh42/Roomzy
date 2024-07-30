fe023f5664ffa271405c15850583a49c9c1fab4e
Harsh <tiwariharsh421@gmail.com>
2024-07-30 15:07:45 +0530

    booking cards added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -48,6 +49,12 @@ const App = () => {
             <Route path="/my-hotel" element={
               <Layout>
                 <MyHotels />
+              </Layout>
+            } />
+
+            <Route path="/my-bookings" element={
+              <Layout>
+                <MyBookings />
               </Layout>
             } />
 
