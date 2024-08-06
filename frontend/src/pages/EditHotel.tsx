e5ab62c8291267aa1ed89b38e5c5694892ce68f9
Harsh <tiwariharsh421@gmail.com>
2024-08-06 12:25:56 +0530

    edit hotes error fixed

--- frontend/src/pages/EditHotel.tsx
+++ frontend/src/pages/EditHotel.tsx
@@ -1,19 +1,20 @@
-import { useParams } from "react-router-dom";
 import { useMutation, useQuery } from "react-query";
-import * as apiClient from '../api-client';
+import { useParams } from "react-router-dom";
+import * as apiClient from "../api-client";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
 
 const EditHotel = () => {
     const { hotelId } = useParams();
-
     const { showToast } = useAppContext();
 
-    const { data: hotel } = useQuery("fetchMyHotelById", () => {
-        apiClient.fetchMyHotelById(hotelId || ""), {
-            enabled: !!hotelId, // this query will only run if it gets a valid hotelId.
+    const { data: hotel } = useQuery(
+        "fetchMyHotelById",
+        () => apiClient.fetchMyHotelById(hotelId || ""),
+        {
+            enabled: !!hotelId,
         }
-    });
+    );
 
     const { mutate, isLoading } = useMutation(apiClient.updateMyHotelById, {
         onSuccess: () => {
@@ -30,7 +31,7 @@ const EditHotel = () => {
 
     return (
         <ManageHotelForm hotel={hotel} onSave={handleSave} isLoading={isLoading} />
-    )
-}
+    );
+};
 
 export default EditHotel;
